645e90964d18a7640d132ccb132fcb861930fdcd
peter <1550969027@qq.com>
2018-09-18 18:19:24 +0800

    map search  address

--- src/Address.php
+++ src/Address.php
@@ -7,19 +7,24 @@ use Zhjun\Address\Exceptions\InvalidArgumentException;
 
 class Address
 {
-    protected $key;
+    protected $gKey;//高德API密匙
 
-    protected $type;
+    protected $bKey;//百度API密匙
+
     protected $guzzleOptions = [];
 
-    public function __construct($key='',$type)
+    public function __construct($gKey='',$bKey='')
     {
-        if(!$key){//如果key不存在就读取配置中的key
-            $this->key = $_ENV('MAP_API_KEY');
+        if(!$gKey){//如果$gKey不存在就读取配置中的$gKey
+            $this->gKey = $_ENV['GAO_API_KEY'];
         }else{
-            $this->key = $key;
+            $this->gKey = $gKey;
         }
-        $this->type = $type;
+        if(!$bKey){//如果bKey不存在就读取配置中的bKey
+            $this->bKey = $_ENV['BAI_API_KEY'];
+        }else{
+            $this->bKey = $bKey;
+        }
     }
 
     public function getHttpClient()
@@ -37,58 +42,91 @@ class Address
      *
      */
 
-    public function getAddress($address,$city='',$batch='false',$output='json',$callback='showLocation'){
+    public function getAddress($address,$city='',$tag='',$batch='false',$output='json'){
 
         $gaode_url = 'https://restapi.amap.com/v3/geocode/geo';//高德API服务地址
-        $baidu_url = 'http://api.map.baidu.com/geocoder/v2/';//百度API服务地址
-
-
+        $baidu_url = 'http://api.map.baidu.com/place/v2/search';//百度API服务地址
         if (!\in_array(\strtolower($output), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: '.$output);
         }
 
         // 2. 封装 query 参数，并对空值进行过滤。
-        if($this->type == 'gaode'){
-            $query = array_filter([
-                'key' => $this->key,
-                'address'=>$address,
-                'city' => $city,
-                'batch'=>$batch,
-                'output' => $output,
-            ]);
-        }else{
-            $query = array_filter([
-                'ak' => $this->key,
-                'address'=>$address,
-//                'city' => $city,
-                'output' => $output,
-                'callback'=>$callback,
-            ]);
-        }
+        $gaode_query = array_filter([ //高德地图参数
+            'key' => $this->gKey,
+            'address'=>$address,
+            'city' => $city,
+            'batch'=>$batch,
+            'output' => $output,
+        ]);
+        $baidu_query = array_filter([ //百度地图参数
+            'ak' => $this->bKey,
+            'query'=>$address,
+            'region' => $city,
+            'tag' =>$tag,
+            'output' => $output,
+        ]);
         try {
             // 3. 调用 getHttpClient 获取实例，并调用该实例的 `get` 方法，
             // 传递参数为两个：$url、['query' => $query]，
-            if($this->type == 'gaode'){
-                $response = $this->getHttpClient()->get($gaode_url, [
-                    'query' => $query,
-                ])->getBody()->getContents();
-                // 4. 返回值根据 $output 返回不同的格式，
-                // 当 $output 为 json 时，返回数组格式，否则为 xml。
-                return $output === 'json' ? \json_decode($response, true) : $response;
-            }else{
-                $response = $this->getHttpClient()->get($baidu_url, [
-                    'query' => $query,
-                ])->getBody()->getContents();
 
-                var_dump($response);
-                return $response;
-            }
+            $response = $this->getHttpClient()->get($gaode_url, [
+                'query' => $gaode_query,
+            ])->getBody()->getContents();
+            // 4. 返回值根据 $output 返回不同的格式，
+            // 当 $output 为 json 时，返回数组格式，否则为 xml。
+            $gaode_rpe = $output === 'json' ? \json_decode($response, true) : $response;
+            $gaode_loca = $gaode_rpe['geocodes'][0]['location'];
+            $gaode_loca = explode(',',$gaode_loca);
+
+            $response = $this->getHttpClient()->get($baidu_url, [
+                'query' => $baidu_query,
+            ])->getBody()->getContents();
+
+            $baidu_rpe = $output === 'json' ? \json_decode($response, true) : $response;
+            $bai_loca = $baidu_rpe['results'][0]['location'];
+            $distance = $this->get_distance($gaode_loca,$bai_loca);//计算经纬度之间的距离
+            return $distance;
 
         } catch (\Exception $e) {
             // 5. 当调用出现异常时捕获并抛出，消息为捕获到的异常消息，
             // 并将调用异常作为 $previousException 传入。
             throw new HttpException($e->getMessage(),$e->getCode(),$e);
         }
+
+    }
+
+    /**
+     * 根据搜索地址获取
+     */
+    public function search($keywords,$city,$types='',$children=1,$offset=20,$page=1,$output='json',$extensions='all'){
+
+        $gaode_url = 'https://restapi.amap.com/v3/place/text';//高德API服务地址搜索
+
+        if (!\in_array(\strtolower($output), ['xml', 'json'])) {
+            throw new InvalidArgumentException('Invalid response format: '.$output);
+        }
+        if (!\in_array(\strtolower($extensions), ['base', 'all'])) {
+            throw new InvalidArgumentException('Invalid type value(base/all): '.$extensions);
+        }
+
+        $gaode_query = array_filter([ //高德地图参数
+            'key' => $this->gKey,
+            'keywords'=>$keywords,//地址
+            'types'=>$types,//类型
+            'city' => $city,//市
+            'children'=>$children,
+            'offset'=>$offset,//每页记录数据
+            'page'=>$page,//页数
+            'output' => $output,
+            'extensions' => $extensions,
+        ]);
+        $response = $this->getHttpClient()->get($gaode_url, [
+            'query' => $gaode_query,
+        ])->getBody()->getContents();
+        // 4. 返回值根据 $output 返回不同的格式，
+        // 当 $output 为 json 时，返回数组格式，否则为 xml。
+        $gaode_rpe = $output === 'json' ? \json_decode($response, true) : $response;
+        return $gaode_rpe;
 
     }
 
@@ -128,4 +166,26 @@ class Address
         }
     }
 
+    /**
+     * @param $from
+     * @param $to
+     * @param bool $km
+     * @param int $decimal
+     * @return float
+     * 计算经纬度距离
+     */
+    public function get_distance($from, $to, $km = true, $decimal = 2) {
+        $from = array_values($from);
+        $to = array_values($to);
+
+        sort($from);
+        sort($to);
+        $EARTH_RADIUS = 6370.996; // 地球半径系数
+        $distance = $EARTH_RADIUS * 2 * asin(sqrt(pow(sin(($from[0] * pi() / 180 - $to[0] * pi() / 180) / 2), 2) + cos($from[0] * pi() / 180) * cos($to[0] * pi() / 180) * pow(sin(($from[1] * pi() / 180 - $to[1] * pi() / 180) / 2), 2))) * 1000;
+
+        if ($km) {
+            $distance = $distance;
+        }
+        return round($distance, $decimal);
+    }
 }
--- tests/AdressTest.php
+++ tests/AdressTest.php
@@ -47,27 +47,11 @@ class AdressTest extends TestCase
     public function testGetAddress()
     {
         // 创建模拟接口响应值。
-        $response = new Response(200, [], '{"success": true}');
-
-        // 创建模拟 http client。
-        $client = \Mockery::mock(Client::class);
-
-        // 指定将会产生的形为（在后续的测试中将会按下面的参数来调用）。
-        $client->allows()->get('https://restapi.amap.com/v3/geocode/geo', [
-            'query' => [
-                'key' => 'mock-key',
-                'address'=>'方恒国际中心A座',
-                'city' => '北京',
-                'batch'=>'false',
-                'output' => 'json',
-            ]
-        ])->andReturn($response);
-
-        // 将 `getHttpClient` 方法替换为上面创建的 http client 为返回值的模拟方法。
-        $w = \Mockery::mock(Address::class, ['mock-key'])->makePartial();
-        $w->allows()->getHttpClient()->andReturn($client); // $client 为上面创建的模拟实例。
-        // 然后调用 `getWeather` 方法，ååå。
-        $this->assertSame(['success' => true], $w->getAddress('方恒国际中心A座','北京'));
+        $gKey = '9422276473854ff6d8e340df63d675f4';
+        $bKey = 'KNb96c0YpWlwIKt5KFIewTPURL4Cnl6G';
+        $w = new Address($gKey,$bKey);
+        $s = $w->getAddress('北京大学','高德院校','北京');
+        $this->assertLessThanOrEqual(500,$s);
     }
 
 
@@ -146,5 +130,35 @@ class AdressTest extends TestCase
         $this->expectException(HttpException::class);
         $this->expectExceptionMessage('request timeout');
         $w->getLocation('116.481488,39.990464');
+    }
+
+    public function testGetSearch()
+    {
+        // 创建模拟接口响应值。
+        $response = new Response(200, [], '{"success": true}');
+
+        // 创建模拟 http client。
+        $client = \Mockery::mock(Client::class);
+
+        // 指定将会产生的形为（在后续的测试中将会按下面的参数来调用）。
+        $client->allows()->get('https://restapi.amap.com/v3/place/text', [
+            'query' => [
+                'key' => 'mock-key',
+                'keywords'=>'北京大学',//地址
+                'types'=>'高校院校',//类型
+                'city' => '北京',//市
+                'children'=>1,
+                'offset'=>20,//每页记录数据
+                'page'=>1,//页数
+                'output' => 'json',
+                'extensions' => 'all',
+            ]
+        ])->andReturn($response);
+
+        // 将 `getHttpClient` 方法替换为上面创建的 http client 为返回值的模拟方法。
+        $w = \Mockery::mock(Address::class, ['mock-key'])->makePartial();
+        $w->allows()->getHttpClient()->andReturn($client); // $client 为上面创建的模拟实例。
+        // 然后调用 `getWeather` 方法，ååå。
+        $this->assertLessThanOrEqual(['success' => true], $w->search('北京大学','高校院校','北京'));
     }
 }
