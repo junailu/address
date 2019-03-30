f10120362e9238ab69296659c10161bb6ede402a
peter <30385934+junailu@users.noreply.github.com>
2019-03-30 10:38:14 +0800

    Update Address.php

--- src/Address.php
+++ src/Address.php
@@ -761,6 +761,9 @@ class Address
          if(!$data['baidu'] || !$data['qmap']){
             return $data['amap'];
         }
+         if(!$data['amap'] && !$data['baidu']){
+            return [];
+        }
         $coordinate_amap    = new \Location\Coordinate(array_get($data, "amap.lat"), array_get($data, "amap.lng"));
         $coordinate_baidu   = new \Location\Coordinate(array_get($data, "baidu.lat"), array_get($data, "baidu.lng"));
 
